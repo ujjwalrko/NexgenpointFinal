2681e57e1cc183ca27001af633f5a0c8e1e893ec
ujjwalrko <dr0908766@gmail.com>
2025-12-01 12:49:25 +0530

    Delete src directory

--- src/components/Footer.jsx
+++
@@ -1,8 +0,0 @@
-export default function Footer() {
-  const year = new Date().getFullYear();
-  return (
-    <footer className="site-footer">
-      <p>© <span id="year">{year}</span> NexGen Point. All rights reserved.</p>
-    </footer>
-  );
-}
